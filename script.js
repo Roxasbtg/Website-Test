e483628245522ac2e75238d0854bd8eab593987c
Roxasbtg <Roxasbtg123@gmail.com>
2022-02-03 15:11:22 -0500

    Fixed getElementById

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function myFunction(){
 
-    document.getElementById("tester") = "Hi, Yana - san. My boyfriend you!" 
+    document.getElementById("tester").innerHTML = "Hi, Yana - san. My boyfriend you!" 
 
 }
